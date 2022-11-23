99a800b8f14b674b177891eef08809bf0143a9bd
QRICK <1462492739@qq.com>
2022-11-23 23:02:25 +0800

    asd
    asda

---
+++ loguse.py
@@ -0,0 +1,13 @@
+from MyLog.logfun import logger_config
+import datetime
+logger=logger_config(r"C:\Users\djc\Documents\GitHub\Log\logpathj",
+                                 "mylog.log",
+                                 "dataSerizer")
+e="xasdada"
+logger.error(e + " " + str("") + " " + str(datetime.time))
+logger.error(e + " " + str("") + " " + str(datetime.time))
+logger.error(e + " " + str("") + " " + str(datetime.time))
+logger.error(e + " " + str("") + " " + str(datetime.time))
+logger.error(e + " " + str("") + " " + str(datetime.time))
+logger.error(e + " " + str("") + " " + str(datetime.time))
+logger.error(e + " " + str("") + " " + str(datetime.time))
